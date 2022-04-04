fa93c83e24dc779301a898e7fcf19a779dfae40b
Administrator <vinodbandari55@gmail.com>
2022-04-04 10:43:42 +0530

    Exception Handling

---
+++ Ecommerce/src/main/java/com/ecommerce/cjss/Ecommerce/exceptions/ErrorCode.java
@@ -0,0 +1,12 @@
+package com.ecommerce.cjss.Ecommerce.exceptions;
+
+public class ErrorCode {
+
+     Integer errorCode;
+     String message;
+
+    public ErrorCode(Integer errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
